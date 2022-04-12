6d09c38a5f80ec61dd87226c6c5463f8b2b1c1e3
Illia Kyselov <illia.kyselov@nimble.com>
2022-04-13 02:33:43 +0300

    init project and 1-project

---
+++ src/1-project/index.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let friends = [];
+    export let clearFiendsList = () => {};
+</script>
+
+<section class="container">
+    <h3>{friends.length} birthdays today</h3>
+
+    {#each friends as friend}
+        <article class="person">
+            <img src="{friend.imgUrl}" alt="{friend.name}">
+            <div>
+                <h4>{friend.name}</h4>
+                <p>{friend.details}</p>
+            </div>
+        </article>
+    {/each}
+
+    <button on:click={clearFiendsList}>clear all</button>
+</section>
---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Board from './1-project/index.svelte';
+
+    const url = 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg';
+
+    let friends = [
+        {name: 'Bertie Yates', details: '29 years'},
+        {name: 'Hester Hogan', details: '29 years'},
+        {name: 'Larry Little', details: '29 years'},
+        {name: 'Sean Walsh', details: '29 years'},
+        {name: 'Lola Gardner', details: '29 years'},
+    ].map(item => ({...item, imgUrl: url}));
+
+    const clearFiendsList = () => friends = [];
+</script>
+
+<main>
+    <Board {friends} {clearFiendsList} />
+</main>
+
+<style>
+	body {
+		width: 100%;
+		height: 100%;
+		background: #ff3e00;
+	}
+
+	.board {
+		width: 360px;
+		min-height: 200px;
+		background-color: #FFFFFF;
+		border-radius: 5px;
+		box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, .1);
+	}
+</style>

b667a2e217233ae09efd8f2a7d6049ee7165ab87
Illia Kyselov <illia.kyselov@nimble.com>
2022-04-14 22:29:19 +0300

    save fourth project

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,10 @@
     // import FirstProject from './1-project/index.svelte';
     // import SecondProject from './2-project/index.svelte';
     // import ThirdProject from './3-project/index.svelte';
-    import ForthProject from './4-project/index.svelte';
+    import FourthProject from './4-project/index.svelte';
 </script>
 
 <!--<FirstProject />-->
 <!--<SecondProject />-->
 <!--<ThirdProject />-->
-<ForthProject />
+<FourthProject />

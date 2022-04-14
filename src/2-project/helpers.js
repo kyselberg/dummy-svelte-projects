aa7796be32e7f92e775a05b69978d02738fe6902
Illia Kyselov <69676320+kyselberg@users.noreply.github.com>
2022-04-14 21:54:51 +0300

    3 project (#2)
    * fix on 2 project
    
    * added original styles
    
    * added original styles
    
    * 3d project
    
    Co-authored-by: Illia Kyselov <illia.kyselov@nimble.com>

--- src/2-project/helpers.js
+++ src/2-project/helpers.js
@@ -1,10 +1,11 @@
 export const DELETE_TOUR_CONTEXT_KEY = 'DELETE_TOUR_CONTEXT_KEY';
 
 export const fetchTours = async ({setIsLoading, setTours}) => {
+    setIsLoading(true);
     try {
-        setIsLoading(true);
         const response = await fetch('https://course-api.com/react-tours-project');
         setTours(await response.json())
+        setIsLoading(false);
     } catch (e) {
         setIsLoading(false);
     }
